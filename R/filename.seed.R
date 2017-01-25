d541c6405fb1dfce420f6154ab327e3e6ca364d0
Timothy Mak <tshmak@hku.hk>
2017-01-25 14:35:24 +0800

    allows filename.seed to retrun more than 1 arguments

--- R/filename.seed.R
+++ R/filename.seed.R
@@ -1,33 +1,34 @@
 filename.seed <- function(filename="test", seed=NULL, prompt=T) {
-  
+
   # Returns "filename.###" as a string where ### is a random number seed
   #         ### is the time of day to the nearest centi-second (or so)
-  #         unless it is specified. 
-  # Also runs set.seed(###) 
+  #         unless it is specified.
+  # Also runs set.seed(###)
   # Typical use:  filename <- filename.seed("MySimulation")
   #               ...
   #               save.image(paste0(filename, ".RData"))
   # Will also take the first argument when run from Rscript as the seed
-  # e.g. run from shell: 
+  # e.g. run from shell:
   #       Rscript myscript.R 245
-  # If myscript includes filename.seed(), this will set the seed to 245 
-  #   have the script run. 
+  # If myscript includes filename.seed(), this will set the seed to 245
+  #   have the script run.
 
   if(is.null(seed)) {
     seed  <- as.integer((as.numeric(Sys.time()) %% 86400) * 100)
     if(interactive() && prompt) {
-      seed2 <- readline("seed? ") 
-      if(seed2 != "") seed <- seed2
+      input <- strsplit(readline("seed? "), "\\s+")
+      if(input != "") seed <- input[1]
     } else {
-      shellinput <- commandArgs(T)
-      if(length(shellinput) > 0) seed <- shellinput[1] 
+      input <- commandArgs(T)
+      if(length(input) > 0) seed <- input[1]
     }
-  } 
-   
+  }
+
   set.seed(seed)
   toreturn <- paste(filename,seed,sep=".")
   attr(toreturn, "seed") <- seed
   attr(toreturn, "filestub") <- filename
+  attr(toreturn, "input") <- input
   return(toreturn)
-  
+
 }
