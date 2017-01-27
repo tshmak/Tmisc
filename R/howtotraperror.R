78675ffe908a0d440d1d8891830bba859c8f8c4a
Timothy Mak <tshmak@hku.hk>
2017-01-27 13:16:14 +0800

    corrects howtotraperror()

--- R/howtotraperror.R
+++ R/howtotraperror.R
@@ -1,7 +1,7 @@
 howtotraperror <- function() {
   cat("withCallingHandlers({\n.\n.\n.\n")
-  cat("}, error=function(e) save.image(paste0(filename, ".error.dump.RData")))")
+  cat("}, error=function(e) save.image(paste0(filename, '.error.dump.RData')))")
 }
-  
+
 
 
