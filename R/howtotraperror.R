3a8726ca11942002f2d6cc191519a9a27f02b3e6
Timothy Mak <tshmak@hku.hk>
2017-01-27 13:19:26 +0800

    amends howtotraperror.R

--- R/howtotraperror.R
+++ R/howtotraperror.R
@@ -1,6 +1,6 @@
 howtotraperror <- function() {
   cat("withCallingHandlers({\n.\n.\n.\n")
-  cat("}, error=function(e) save.image(paste0(filename, '.error.dump.RData')))")
+  cat("}, error=function(e) save.image(paste0(*******, '.error.dump.RData')))")
 }
 
 
