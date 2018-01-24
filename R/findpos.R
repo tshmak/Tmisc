c618554447df2c05fc1c67b249175747af9edb09
Timothy Mak <tshmak@hku.hk>
2018-01-24 11:39:43 +0800

    adds features

--- R/findpos.R
+++ R/findpos.R
@@ -1,9 +1,10 @@
-findpos <- function(vec, within=NULL) {
+findpos <- function(vec, within=NULL, firstonly=FALSE, lastonly=FALSE) {
   #' Function to find position of TRUEs in a logical vector. 
   #' You can optionally put in a "within" vector to find the positions
   #' within the "within" vector. 
   #' @param vec A logical vector or a findpos object
   #' @param within A logical vector or a findpos object
+  stopifnot(!firstonly || !lastonly)
   if(class(vec) == "findpos") {
     vec.len <- attr(vec, "vector.length")
   } else if(is.logical(vec)) {
@@ -15,13 +16,17 @@ findpos <- function(vec, within=NULL) {
   if(!is.null(within)) {
     within.pos <- findpos(within)
     stopifnot(vec.len == length(within.pos))
-    return(as.findpos(within.pos[vec], attr(within.pos, "vector.length")))
+    vec.len <- attr(within.pos, "vector.length")
+    res <- within.pos[vec]
   } else if(class(vec) == "findpos") {
-    return(vec)
+    res <- vec
   } else if(is.logical(vec)) {
     res <- which(vec)
-    attr(res, "vector.length") <- length(vec)
-    class(res) <- "findpos"
-    return(res)
   }
+  
+  if(firstonly) res <- res[1] else if(lastonly) res <- res[length(res)]
+  attr(res, "vector.length") <- length(vec)
+  class(res) <- "findpos"
+  return(res)
+  
 }
